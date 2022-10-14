10354ee74ccdf8a7bf17265204a27013cdbe2836
Dominic Sayers <dominic@sayers.cc>
2022-10-14 14:51:02 +0100

    Remove unused fitvids component

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,14 +8,12 @@
 
     $(document).ready(function(){
 
-        $(".post-content").fitVids();
-        
         // Calculates Reading Time
         $('.post-content').readingTime({
             readingTimeTarget: '.post-reading-time',
             wordCountTarget: '.post-word-count',
         });
-        
+
         // Creates Captions from Alt tags
         $(".post-content img").each(function() {
             // Let's put a caption if there is one
@@ -23,7 +21,7 @@
               $(this).wrap('<figure class="image"></figure>')
               .after('<figcaption>'+$(this).attr("alt")+'</figcaption>');
         });
-        
+
     });
 
 }(jQuery));
