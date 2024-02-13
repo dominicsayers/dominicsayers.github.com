71cffbce485151bd128780c56eeeba82d65e9af0
Dominic Sayers <dominic@sayers.cc>
2024-02-13 14:20:50 +0000

    Remove unsupported meta tag

--- source/is_email/test/index.php
+++ source/is_email/test/index.php
@@ -2,7 +2,6 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="chrome=1"/>
 	<title>Testing is_email()</title>
 	<link rel="stylesheet" href="tests.css"/>
 </head>
